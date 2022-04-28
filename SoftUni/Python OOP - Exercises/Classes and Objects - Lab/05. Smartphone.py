17b9c087776a1498520a32842b9d13b47101738c
iceburned <teodor.vulev@gmail.com>
2022-04-28 21:47:32 +0300

    update

--- SoftUni/Python OOP - Exercises/Classes and Objects - Lab/05. Smartphone.py
+++ SoftUni/Python OOP - Exercises/Classes and Objects - Lab/05. Smartphone.py
@@ -1,24 +1,20 @@
 class Smartphone:
-    def __init__(self, memory, apps, is_on):
+    def __init__(self, memory):
         self.memory = memory
-        self.apps = apps
-        self.is_on = bool(is_on)
+        self.apps = []
+        self.is_on = False
 
     def power(self):
-        if self.is_on:
-            self.is_on = False
-        else:
-            self.is_on = True
+        self.is_on = not self.is_on
 
     def install(self, app, app_memory):
-        if self.memory >= app_memory and self.is_on:
-            self.apps.append(app)
-            self.memory -= app_memory
-            return f"Installing {app}"
-        elif self.memory >= app_memory and not self.is_on:
-            return f"Not enough memory to install {app}"
-        else:
-            f"Not enough memory to install {app}"
+        if self.memory >= app_memory:
+            if self.is_on:
+                self.apps.append(app)
+                self.memory -= app_memory
+                return f"Installing {app}"
+            return f"Turn on your phone to install {app}"
+        return f"Not enough memory to install {app}"
 
     def status(self):
         return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
