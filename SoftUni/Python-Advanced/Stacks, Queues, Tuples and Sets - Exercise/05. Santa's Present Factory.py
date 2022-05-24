67c2feef4605c0570fc03a21c1870fa39c7f69e0
Teodor Vulev <teodor.vulev@gmail.com>
2022-05-25 00:27:10 +0300

    change

--- SoftUni/Python-Advanced/Stacks, Queues, Tuples and Sets - Exercise/05. Santa's Present Factory.py
+++ SoftUni/Python-Advanced/Stacks, Queues, Tuples and Sets - Exercise/05. Santa's Present Factory.py
@@ -1,78 +1,83 @@
-from collections import deque
+# spageti code, but gives 100/100
 
-
-class SantaPresent:
-    dkt = {150: "Doll",
-           250: "Wooden train",
-           300: "Teddy bear",
-           400: "Bicycle"}
-
-    def __init__(self, materials, magic):
-        self.materials = materials
-        self.magic = magic
-        self.temp_box = {}
-        self.flag = False
-        SantaPresent.maincycle(self, materials, magic)
-
-    def maincycle(self, materials, magic):
-        while materials and magic:
-            self.zero_or_one()
-            if not materials or not magic:
-                break
-            sum_sum = materials[-1] * magic[0]
-            if sum_sum <= 0:
-                self.if_negative_number()
-            elif sum_sum in SantaPresent.dkt:
-                self.add_to_temp_box(sum_sum)
-                self.remove_material_magic()
-            elif sum_sum not in SantaPresent.dkt:
-                self.not_in_dkt()
-        self.flag_mark()
-        self.print_output()
-
-    def add_to_temp_box(self, sum_sum):
-        if SantaPresent.dkt[sum_sum] not in self.temp_box:
-            self.temp_box[SantaPresent.dkt[sum_sum]] = 0
-        self.temp_box[SantaPresent.dkt[sum_sum]] += 1
-
-    def remove_material_magic(self):
-        self.materials.pop()
-        self.magic.popleft()
-
-    def if_negative_number(self):
-        subtract = self.materials[-1] + magic[0]
-        self.remove_material_magic()
-        self.materials.append(subtract)
-
-    def not_in_dkt(self):
-        self.magic.popleft()
-        self.materials[-1] += 15
-
-    def zero_or_one(self):
-        if self.materials[-1] == 0:
-            self.materials.pop()
-        elif self.magic[0] == 0:
-            self.magic.popleft()
-
-    def flag_mark(self):
-        first_pair = "Doll" in self.temp_box and "Wooden train" in self.temp_box
-        second_pair = "Teddy bear" in self.temp_box and "Bicycle" in self.temp_box
-        if first_pair or second_pair:
-            self.flag = True
-
-    def print_output(self):
-        if self.flag:
-            print("The presents are crafted! Merry Christmas!")
-        else:
-            print("No presents this Christmas!")
-        if self.materials:
-            print(f"Materials left: {', '.join(map(str, self.materials))}")
-        if self.magic:
-            print(f"Magic left: {', '.join(map(str, self.magic))}")
-        for k,v in self.temp_box.items():
-            print(f"{k}: {v}")
-
-
-materials = deque(int(_) for _ in input().split())
-magic = deque(int(_) for _ in input().split())
-a = SantaPresent(materials, magic)
+# from collections import deque
+#
+#
+# class SantaPresent:
+#     dkt = {150: "Doll",
+#            250: "Wooden train",
+#            300: "Teddy bear",
+#            400: "Bicycle"}
+#
+#     def __init__(self, materials, magic):
+#         self.materials = materials
+#         self.magic = magic
+#         self.temp_box = {}
+#         self.flag = False
+#         SantaPresent.maincycle(self, materials, magic)
+#
+#     def maincycle(self, materials, magic):
+#         while materials and magic:
+#             if materials[-1] == 0 or self.magic[0] == 0:
+#                 self.zero_or_one()
+#                 continue
+#             sum_sum = materials[-1] * magic[0]
+#             if sum_sum <= 0:
+#                 self.if_negative_number()
+#             elif sum_sum in SantaPresent.dkt:
+#                 self.add_to_temp_box(sum_sum)
+#                 self.remove_material_magic()
+#             elif sum_sum not in SantaPresent.dkt:
+#                 self.not_in_dkt()
+#         self.flag_mark()
+#         self.print_output()
+#
+#     def add_to_temp_box(self, sum_sum):
+#         if SantaPresent.dkt[sum_sum] not in self.temp_box:
+#             self.temp_box[SantaPresent.dkt[sum_sum]] = 0
+#         self.temp_box[SantaPresent.dkt[sum_sum]] += 1
+#
+#     def remove_material_magic(self):
+#         self.materials.pop()
+#         self.magic.popleft()
+#
+#     def if_negative_number(self):
+#         subtract = self.materials[-1] + magic[0]
+#         self.remove_material_magic()
+#         self.materials.append(subtract)
+#
+#     def not_in_dkt(self):
+#         self.magic.popleft()
+#         self.materials[-1] += 15
+#
+#     def zero_or_one(self):
+#         if self.materials[-1] == 0 and self.magic[0] == 0:
+#             self.materials.pop()
+#             self.magic.popleft()
+#         elif self.magic[0] == 0:
+#             self.magic.popleft()
+#         elif self.materials[-1] == 0:
+#             self.materials.pop()
+#
+#     def flag_mark(self):
+#         first_pair = "Doll" in self.temp_box and "Wooden train" in self.temp_box
+#         second_pair = "Teddy bear" in self.temp_box and "Bicycle" in self.temp_box
+#         if first_pair or second_pair:
+#             self.flag = True
+#
+#     def print_output(self):
+#         if self.flag:
+#             print("The presents are crafted! Merry Christmas!")
+#         else:
+#             print("No presents this Christmas!")
+#         if self.materials:
+#             print(f"Materials left: {(', '.join(map(str, reversed(self.materials))))}")
+#         if self.magic:
+#             print(f"Magic left: {', '.join(map(str, self.magic))}")
+#         for k, v in sorted(self.temp_box.items()):
+#             print(f"{k}: {v}")
+#
+#
+# materials = deque(int(_) for _ in input().split())
+# magic = deque(int(_) for _ in input().split())
+# a = SantaPresent(materials, magic)
