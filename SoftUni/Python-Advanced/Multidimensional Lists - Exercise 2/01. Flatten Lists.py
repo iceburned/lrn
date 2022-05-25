a43dd318c5ff781340e4c9512cf8de9453f72e78
Teodor Vulev <teodor.vulev@gmail.com>
2022-05-26 01:15:08 +0300

    change

--- SoftUni/Python-Advanced/Multidimensional Lists - Exercise 2/01. Flatten Lists.py
+++ SoftUni/Python-Advanced/Multidimensional Lists - Exercise 2/01. Flatten Lists.py
@@ -1,2 +1,4 @@
-data = [_ for _ in input().split("|")]
-a = 1
+data = [_.strip().split() for _ in input().split("|")]
+for s in range(len(data)-1, -1, -1):
+    for ss in data[s]:
+        print(ss, end=' ')
