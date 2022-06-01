72927c8e0b6a8785e9b3d12983267bb6ed844146
Teodor Vulev <teodor.vulev@gmail.com>
2022-06-01 10:08:10 +0300

    change

--- SoftUni/Python-Advanced/Functions Advanced - Exercise/08. Age Assignment.py
+++ SoftUni/Python-Advanced/Functions Advanced - Exercise/08. Age Assignment.py
@@ -1,14 +1,12 @@
 def age_assignment(*args, **kwargs):
     names = args
     age = kwargs
-    # dkt = {}
-    # for n in names:
-    #     if n[0] in age:
-    #         dkt.update({n: age[n[0]]})
-    # dkt = dict(sorted(dkt.items()))
-    # return [''.join(f"{k} is {v} years old.") for k, v in dkt.items()]
-    dkt = {x: age[x[0]] for x in names}
-    dkt = dict(sorted(dkt.items()))
+    dkt = {}
+    for n in names:
+        if n[0] in age:
+            dkt.update({n: age[n[0]]})
+    sorted_result = [f"{k} is {v} years old." for k, v in sorted(dkt.items(), key=lambda x: x[0])]
+    return '\n'.join(sorted_result)
 
 
 print(age_assignment("Peter", "George", G=26, P=19))
