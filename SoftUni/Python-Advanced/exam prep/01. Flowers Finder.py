74c11a0756cf8ed1b6b4c9cc1580abcaedff8951
Teodor Vulev <teodor.vulev@gmail.com>
2022-06-02 18:03:20 +0300

    change

---
+++ SoftUni/Python-Advanced/exam prep/01. Flowers Finder.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+vowels = deque(_ for _ in input().split())
+consonants = deque(_ for _ in input().split())
+words = ["rose", "tulip", "lotus", "daffodil"]
+flag  =True
+while vowels and consonants and flag:
+    vowel = vowels.popleft()
+    consonant = consonants.pop()
+    # words = words.popleft()
+    for i in range(4):
+        if vowel in words[i]:
+            words[i] = words[i].replace(vowel, '', 1)
+    for ii in range(4):
+        if consonant in words[ii]:
+            words[ii] = words[ii].replace(consonant, '', 1)
+    if not all(filter(None, words)):
+        break
