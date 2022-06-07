c7bea167deedf86bad5c78b58792929bf61781c2
Teodor Vulev <teodor.vulev@gmail.com>
2022-06-07 22:05:14 +0300

    add/change

--- SoftUni/Python-Advanced/Error Handling/Numbers Dictionary.py
+++ SoftUni/Python-Advanced/Error Handling/Numbers Dictionary.py
@@ -1,34 +1,56 @@
-numbers_dictionary = {}
+"""
+input:
+one
+1
+two
+2
+Search
+one
+Remove
+two
+End
+"""
 
+numbers_dictionary = {}   # temp dictionary
 line = input()
 
+# condition until Search
 while line != "Search":
     try:
         number_as_string = line
         number = int(input())
         numbers_dictionary[number_as_string] = number
-    except ValueError:
+    except ValueError:                                     # ValueError
         print("The variable number must be an integer")
     line = input()
 
 line = input()
 
+# condition until Remove
 while line != "Remove":
     try:
         searched = line
         print(numbers_dictionary[searched])
-    except KeyError:
+    except KeyError:                                        # KeyError
         print("Number does not exist in dictionary")
     line = input()
 
 line = input()
 
+# condition until End
 while line != "End":
     try:
         searched = line
         del numbers_dictionary[searched]
-    except KeyError:
+    except KeyError:                                        # KeyError
         print("Number does not exist in dictionary")
     line = input()
 
 print(numbers_dictionary)
+
+
+'''
+Output:
+1
+{'one': 1}
+'''
