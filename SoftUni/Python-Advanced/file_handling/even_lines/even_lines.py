8f5321f0ee39e8f2cff4ac53194883a70c07991b
Teodor Vulev <teodor.vulev@gmail.com>
2022-06-13 20:18:32 +0300

    add/change

---
+++ SoftUni/Python-Advanced/file_handling/even_lines/even_lines.py
@@ -0,0 +1,18 @@
+text_file = open("text.txt", "r")  # open file text.txt
+flag = 0
+for line in text_file:
+    if flag % 2 == 0:
+        for s in line:
+            if s in ("-", ",", ".", "!", "?"):
+                line = line.replace(s, "@")
+        line = line.split()
+        print(*line[::-1])
+    flag += 1
+text_file.close()  # close file text.txt
+
+
+'''
+output:
+fault@ his wasn't it but him@ judge to quick was @I
+safer@ is It here@ hide @Quick@
+'''
