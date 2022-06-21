8cda2a3d40089824354a0b7977c085e7683afa60
Teodor Vulev <teodor.vulev@gmail.com>
2022-06-21 21:35:23 +0300

    add/change

---
+++ SoftUni/Python-Advanced/exam prep/02. Ball in the Bucket.py
@@ -0,0 +1,39 @@
+mtx = [[s for s in input().split()] for _ in range(6)]
+
+
+def trow(cords, board):
+    num = tuple(map(int, cords.strip('()').split(', ')))
+    flag = False
+    if (0 <= num[0] < 6 or 0 <= num[1] < 6) and board[num[0]][num[1]] == 'B':
+        sum_sum = 0
+        flag = True
+        for i in range(6):
+            if board[i][num[1]].isalpha():
+                pass
+            else:
+                sum_sum += int(board[i][num[1]])
+        return sum_sum, flag
+    return 0, flag
+
+
+checker = []
+scores = 0
+for _ in range(3):
+    num = input()
+
+    if num not in checker:
+        points, flag = trow(num, mtx)
+        scores += points
+        if flag:
+            checker.append(num)
+    else:
+        scores += 0
+
+if scores >= 300:
+    print(f"Good job! You scored {scores} points, and you've won Lego Construction Set.")
+elif 200 <= scores <= 299:
+    print(f"Good job! You scored {scores} points, and you've won Teddy Bear.")
+elif 100 <= scores <= 199:
+    print(f"Good job! You scored {scores} points, and you've won Football.")
+elif scores < 100:
+    print(f"Sorry! You need {100 - scores} points more to win a prize.")
