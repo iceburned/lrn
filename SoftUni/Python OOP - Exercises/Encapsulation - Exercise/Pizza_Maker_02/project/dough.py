188b5d39fca34b399f25863a51119fd63e4372a9
Teodor Vulev <teodor.vulev@gmail.com>
2022-07-01 16:16:25 +0300

    add/change

--- SoftUni/Python OOP - Exercises/Encapsulation - Exercise/Pizza_Maker_02/project/dough.py
+++ SoftUni/Python OOP - Exercises/Encapsulation - Exercise/Pizza_Maker_02/project/dough.py
@@ -30,6 +30,6 @@ class Dough:
 
     @weight.setter
     def weight(self, value):
-        if value == 0:
+        if value <= 0:
             raise ValueError("The weight cannot be less or equal to zero")
         self.__weight = value
--- SoftUni/Python OOP - Exercises/Encapsulation - Exercise/Pizza_Maker_02/project/pizza.py
+++ SoftUni/Python OOP - Exercises/Encapsulation - Exercise/Pizza_Maker_02/project/pizza.py
@@ -3,7 +3,7 @@ from project.topping import Topping
 
 
 class Pizza:
-    def __init__(self, name: str, dough, toppings_capacity: int):
+    def __init__(self, name: str, dough: Dough, toppings_capacity: int):
         self.name = name
         self.dough = dough
         self.toppings_capacity = toppings_capacity
@@ -40,11 +40,12 @@ class Pizza:
         self.__toppings_capacity = value
 
     def add_topping(self, topping: Topping):
-        if len(self.toppings) >= self.toppings_capacity:
-            raise "Not enough space for another topping"
-        if topping.topping_type not in self.toppings:
-            self.toppings[topping.topping_type] = 0
-        self.toppings[topping.topping_type] += topping.weight
+        if len(self.toppings) == self.toppings_capacity:
+            raise ValueError("Not enough space for another topping")
+        if topping.topping_type in self.toppings:
+            self.toppings[topping.topping_type] += topping.weight
+        else:
+            self.toppings[topping.topping_type] = topping.weight
 
     def calculate_total_weight(self):
         return sum(self.toppings.values()) + self.dough.weight
