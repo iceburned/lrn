2d544472d14ed50e5361197795c8953b05d14f97
Teodor Vulev <teodor.vulev@gmail.com>
2022-07-06 17:21:14 +0300

    add/change

--- SoftUni/Python OOP - Exercises/static_and_class_methods_lab/hotel_room_04/project/hotel.py
+++ SoftUni/Python OOP - Exercises/static_and_class_methods_lab/hotel_room_04/project/hotel.py
@@ -5,7 +5,10 @@ class Hotel:
     def __init__(self, name):
         self.name = name
         self.rooms = []
-        self.guests = 0
+
+    @property
+    def guests(self):
+        return sum(_.guests for _ in self.rooms)
 
     @classmethod
     def from_stars(cls, stars_count):
@@ -17,21 +20,17 @@ class Hotel:
     def take_room(self, room_number, people):
         for r in self.rooms:
             if r.number == room_number:
-                if r.capacity >= people + r.guests:
-                    r.guests += people
-                    r.is_taken = True
+                return r.take_room(people)
 
     def free_room(self, room_number):
         for r in self.rooms:
             if r.number == room_number:
-                r.guests = 0
-                r.is_taken = False
+                return r.free_room()
 
     def status(self):
-        all_guests = sum(_.guests for _ in self.rooms)
         free = []
         taken = []
-        [taken.append(_.number) if _.is_taken else free.append(_.number) for _ in self.rooms]
-        return f"Hotel {self.name} has {all_guests} total guests\n" \
-               f"Free rooms: {', '.join(map(str, free))}\n" \
-               f"Taken rooms: {', '.join(map(str, taken))}"
+        [taken.append(str(_.number)) if _.is_taken else free.append(str(_.number)) for _ in self.rooms]
+        return f"Hotel {self.name} has {self.guests} total guests\n" \
+               f"Free rooms: {', '.join(free)}\n" \
+               f"Taken rooms: {', '.join(taken)}"
