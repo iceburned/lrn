91a27f8dc179da510cf750e30b8f15d14acca190
Teodor Vulev <teodor.vulev@gmail.com>
2022-08-03 14:40:31 +0300

    add/change

--- SoftUni/Python OOP - Exercises/exam_prep/Python OOP Exam - 11 December 2021/project/controller.py
+++ SoftUni/Python OOP - Exercises/exam_prep/Python OOP Exam - 11 December 2021/project/controller.py
@@ -14,7 +14,7 @@ class Controller:
         if any(_.model == model for _ in self.cars):
             raise Exception(f"Car {model} is already created!")
         try:
-            car = self.car_factory.car_creation(car_type, model,  speed_limit)
+            car = self.car_factory.car_creation(car_type, model, speed_limit)
             self.cars.append(car)
             return f"{car.__class__.__name__} {car.model} is created."
         except RuntimeError:
@@ -35,22 +35,81 @@ class Controller:
         return f"Race {race.name} is created."
 
     def add_car_to_driver(self, driver_name: str, car_type: str):
-        # driver = Validator.same_name_like_object_name(driver_name, self.drivers)
-        # reversed_cars = reversed(self.cars)
-        # car = Validator.same_model_like_object_model(car_type, reversed_cars)
-        # if any(driver):
-        #     raise Exception(f"Driver {driver_name} could not be found!")
-        # if any(car):
-        #     raise Exception(f"Car {car_type} could not be found!")
-        pass
+        driver = self.__find_driver_by_name(driver_name)
+        if driver is None:
+            raise Exception(f"Driver {driver_name} could not be found!")
+
+        car = self.__find_car_by_type(car_type)
+        if car is None:
+            raise Exception(f"Car {car_type} could not be found!")
+
+        return driver.change_car(car)
 
     def add_driver_to_race(self, race_name: str, driver_name: str):
-        pass
+        race = self.__find_race_by_name(race_name)
+        if race is None:
+            raise Exception( f"Race {race_name} could not be found!")
+
+        driver = self.__find_driver_by_name(driver_name)
+        if driver is None:
+            raise Exception(f"Driver {driver_name} could not be found!")
+
+        if driver.car is None:
+            raise Exception(f"Driver {driver_name} could not participate in the race!")
+
+        driver_have_been_on_race = self.__driver_already_participate_in_race(race_name, driver_name)
+        if driver_have_been_on_race is not None:
+            return driver_have_been_on_race  # Driver {driver_name} is already added in {race_name} race.
+
+        race.drivers.append(driver)
+        return f"Driver {driver_name} added in {race_name} race."
 
     def start_race(self, race_name):
-        pass
+        race = self.__find_race_by_name(race_name)
+        if race is None:
+            raise Exception(f"Race {race_name} could not be found!")
 
-#
-# c = Controller()
-# c.create_car("MuscleCar", "Ford", 400)
-# a = 1
+        if len(race.drivers) < 3:
+            raise Exception(f"Race {race_name} cannot start with less than 3 participants!")
+
+        all_winners = ""
+        winners = self.__first_3_winners_of_race(race)
+        for winner in winners:
+            winner.number_of_wins += 1
+            all_winners += f"Driver {winner.name} wins the {race.name} race with a speed of {winner.car.speed_limit}.\n"
+        return all_winners
+
+
+
+
+
+    def __find_driver_by_name(self, driver_name):
+        for obj in self.drivers:
+            if obj.name == driver_name:
+                return obj
+        return None
+
+    def __find_car_by_type(self, car_type):
+        for index in range(len(self.cars) - 1, -1, -1):
+            car = self.cars[index]
+            if car.__class__.__name__ == car_type and car.is_taken is False:
+                return car
+        return None
+
+    def __find_race_by_name(self, race_name):
+        for obj in self.races:
+            if race_name == obj.name:
+                return obj
+        return None
+
+    def __driver_already_participate_in_race(self, race_name, driver_name):
+        for obj in self.races:
+            for driver in obj.drivers:
+                if driver.name == driver_name:
+                    return f"Driver {driver_name} is already added in {race_name} race."
+        return None
+
+    def __first_3_winners_of_race(self, race):
+        drivers = race.drivers
+        racing = sorted(drivers, key=lambda x: -x.car.speed_limit)
+        return racing[:3]
--- SoftUni/Python OOP - Exercises/exam_prep/Python OOP Exam - 11 December 2021/project/driver.py
+++ SoftUni/Python OOP - Exercises/exam_prep/Python OOP Exam - 11 December 2021/project/driver.py
@@ -1,3 +1,4 @@
+from project.car.car import Car
 from project.core.validator import Validator
 
 
@@ -16,3 +17,15 @@ class Driver:
         Validator.check_for_empty_or_white_space(value,
                                                  "Name should contain at least one character!")
         self.__name = value
+
+    def change_car(self, car: Car):
+        car.is_taken = True
+        if self.car is None:
+            self.car = car
+            return f"Driver {self.name} chose the car {car.model}."
+
+        self.car.is_taken = False
+        old_model = self.car.model
+        self.car = car
+        return f"Driver {self.name} changed his car from {old_model} to {car.model}."
+
